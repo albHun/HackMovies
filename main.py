be971e1e726f255bd96e3dd7f4aa16e81cfe5abb
Albert Jiang <albert594250@gmail.com>
2017-04-01 17:58:30 +0100

    Added main execution file

---
+++ main.py
@@ -0,0 +1,29 @@
+from json import json
+
+from Analytics import findBestAndWorst
+from Analytics import analyze
+
+
+# Opening the json file and parsing it
+with open("data.json") as data_file:
+	data = json.load(data_file)
+
+# Getting the information out from file
+X = data["word vectors"]
+ratings = data["movie ratings"]
+
+# Using functions defined in Analytics.py to briefly analyze the data
+movieIndicesClustered = analyze(X)
+bestIndices, worstIndices = findBestAndWorst(X, ratings)
+
+print("Clusters best movies belong to")
+for i in bestIndices:
+	for cluster, indiceList in movieIndicesClustered.iteritems():
+		if i in indiceList:
+			print(cluster)
+
+print("Clusters worst movies belong to")
+for i in worstIndices:
+	for cluster, indiceList in movieIndicesClustered.iteritems():
+		if i in indiceList:
+			print(cluster)
