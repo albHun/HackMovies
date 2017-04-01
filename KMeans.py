9452e9d5396b8bbcaa7f5472c6d1f8a1c99ea081
Albert Jiang <albert594250@gmail.com>
2017-04-01 16:33:44 +0100

    Added comments for KMeans code

--- KMeans.py
+++ KMeans.py
@@ -3,7 +3,8 @@ import time
 import numpy as nu
 from sklearn.cluster import MiniBatchKMeans
 
-
+# Given a dataset X, return the labels of entries in X
+# Default K = 8
 def KMeansClustering(X):
 	clu = MiniBatchKMeans(n_clusters=8, verbose = True)
 	clu.fit(X)
