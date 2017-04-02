cd99861e0cf74274c61942ed6640c037490f6ede
Albert Jiang <albert594250@gmail.com>
2017-04-02 01:12:38 +0100

    Added preprocessing to scale the data

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+# Special thanks to chuansun76 on kaggle for uploading
+# the IMDB 5000 movie dataset
+
 import os
+# import seaborn as sns
+# sns.set(style='white')
 
 import json
 import pandas as pd
+from pandas.io.json import json_normalize
+from sklearn.preprocessing import MinMaxScaler
 
-from Analytics import findBestAndWorst
-from Analytics import analyze
+#from Analytics import findBestAndWorst
+#from Analytics import analyze
+
+
 """
+# Dumping json that stores everything
 path = 'F:\MyProjects\MLH_prime\data\meta'
 files = list()
 for j in range(1, 251):
@@ -18,12 +28,44 @@ with open("data.json", "w") as outfile:
     json.dump(files, outfile)
 """
 
+# Supervised Learning
+df = pd.read_csv("movie_metadata.csv")
+print(df.shape)
+
+df1 = df.dropna()
+print(df1.shape)
+print(df1.dtypes)
+
+# Eliminating non-numerical fields
+df2 = df1.select_dtypes(include=['int64','float64'])
+# print(df2)
+
+percentageProfit = 100 * (df2['gross'].values - df2['budget'].values) / df2['budget'].values
+# print(percetageProfit[:5])
+df2 = df2.drop('gross', 1)
+df2 = df2.drop('director_facebook_likes', 1)
+
+matrixed = df2.as_matrix()
+
+minMaxScaler = MinMaxScaler()
+df2_scaled = minMaxScaler.fit_transform(df2)
+print(df2_scaled)
 
 
-# Getting the information out from file
+# df2.loc[:,'percentageProfit'] = pd.Series(percentageProfit, index=df2.index)
+# g = sns.jointplot(x="title_year", y="percentageProfit",kind='scatter',size=10,\
+#                   ylim = [0,110],xlim=[1980,2020],data=df2)
+# g = sns.pairplot(df1,hue='content_rating')
+
+# Unsupervised learning
+# Getting the comment information out from file
+
 """
 This waits to be specified with the incoming data.
 """
+
+
+
 
 X = data["word vectors"]
 ratings = data["movie ratings"]
@@ -32,6 +74,8 @@ ratings = data["movie ratings"]
 movieIndicesClustered = analyze(X)
 bestIndices, worstIndices = findBestAndWorst(X, ratings)
 
+
+"""
 print("Clusters best movies belong to")
 for i in bestIndices:
     for cluster, indiceList in movieIndicesClustered.iteritems():
